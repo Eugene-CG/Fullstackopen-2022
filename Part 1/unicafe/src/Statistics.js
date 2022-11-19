2f8addb3249763206be2e4469faaa7ec3e7c18df
Eugene-CG <evhenisssss@gmail.com>
2022-11-19 18:28:29 +0200

    Part 1, d, 1.9

--- Part 1/unicafe/src/Statistics.js
+++ Part 1/unicafe/src/Statistics.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Statistics({ all, average, positive }) {
+  if (all === 0) return <div>No feedback given</div>;
   return (
     <>
       <p>all: {all}</p>
